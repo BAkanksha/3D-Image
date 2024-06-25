075eccb098b48c73b42b67ff00eb0bd52f9e3bae
BAkanksha <akankshasingh4668@gmail.com>
2024-06-25 12:25:19 +0530

    3D-Image

---
+++ script.js
@@ -0,0 +1,30 @@
+const ImageElement=document.querySelector(".image-container");
+
+const prevEle= document.getElementById("prev");
+const nextEle= document.getElementById("next");
+
+let x=0;
+let timer;
+
+function updateGallery(){
+    ImageElement.style.transform=`perspective(1000px) rotateY(${x}deg)`;
+    
+    timer = setTimeout(()=>{
+        x = x - 45;
+        updateGallery();
+    }, 2000);
+}
+ 
+updateGallery();
+
+prevEle.addEventListener("click", ()=>{
+    clearTimeout(timer);
+    x = x + 45;
+    updateGallery();
+});
+
+nextEle.addEventListener("click", ()=>{
+    clearTimeout(timer);
+    x = x - 45;
+    updateGallery();
+});
